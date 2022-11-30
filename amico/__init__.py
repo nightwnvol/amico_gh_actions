c654123c058559d205e13bf65b83aa57025db81e
nightwnvol <clorindo.notte@univr.it>
2022-11-30 10:44:48 +0100

    build: add importlib

--- amico/__init__.py
+++ amico/__init__.py
@@ -8,5 +8,11 @@ from . import lut
 from . import models
 from . import util
 
-from pkg_resources import get_distribution
-__version__ = get_distribution('dmri-amico').version
+# from pkg_resources import get_distribution
+# __version__ = get_distribution('dmri-amico').version
+
+try:
+    from importlib.metadata import version
+except ImportError:
+    from importlib_metadaata import version
+__version__ = version('dmri-amico')
