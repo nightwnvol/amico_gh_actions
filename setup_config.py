0c209c6d131f54ef3f774c056a7717de0bf37426
nightwnvol <clorindo.notte@univr.it>
2023-03-13 18:10:12 +0100

    Update setup_config.py

--- setup_config.py
+++ setup_config.py
@@ -3,10 +3,10 @@ import argparse
 
 # parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('config', type=str)
-parser.add_argument('libraries', type=str)
-parser.add_argument('library_dirs', type=str)
-parser.add_argument('include_dirs', type=str)
+parser.add_argument('--config', type=str)
+parser.add_argument('--libraries', type=str, nargs='+')
+parser.add_argument('--library_dirs', type=str, nargs='+')
+parser.add_argument('--include_dirs', type=str, nargs='+')
 args = parser.parse_args()
 
 # write the site.cfg file
