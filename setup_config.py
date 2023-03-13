c66a9a6546ce3b926fc795054559eeb028aea04c
nightwnvol <clorindo.notte@univr.it>
2023-03-13 18:25:24 +0100

    fix: setup_config.py

--- setup_config.py
+++ setup_config.py
@@ -3,18 +3,18 @@ import argparse
 
 # parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('--config', type=str)
-parser.add_argument('--libraries', type=str, nargs='+')
-parser.add_argument('--library_dirs', type=str, nargs='+')
-parser.add_argument('--include_dirs', type=str, nargs='+')
+parser.add_argument('config', type=str)
+parser.add_argument('library', type=str)
+parser.add_argument('library_dir', type=str)
+parser.add_argument('include_dir', type=str)
 args = parser.parse_args()
 
 # write the site.cfg file
 config = configparser.ConfigParser()
 config[args.config] = {
-    'libraries': args.libraries,
-    'library_dirs': args.library_dirs,
-    'include_dirs': args.include_dirs
+    'library': args.libraries,
+    'library_dir': args.library_dirs,
+    'include_dir': args.include_dirs
 }
 with open('site.cfg', 'a') as configfile:
     config.write(configfile)
