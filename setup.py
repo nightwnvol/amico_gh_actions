58055345e9cd3d9e0ff9426dbabae5cb894faa72
nightwnvol <clorindo.notte@univr.it>
2023-03-20 11:45:07 +0100

    chore: update test

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from Cython.Build import cythonize
 import cyspams
 import sys
 from os import cpu_count
+from os.path import isfile
 import configparser
 
 # parallel compilation
@@ -19,14 +20,16 @@ extra_compile_args = []
 # spams-cython headers
 include_dirs.extend(cyspams.get_include())
 # OpenBLAS headers
+if not isfile('site.cfg'):
+      print(f'\033[31mFileNotFoundError: cannot find the site.cfg file\033[0m')
 config = configparser.ConfigParser()
 config.read('site.cfg')
-if 'openblas' in config:
+try:
       libraries.extend([config['openblas']['library']])
       library_dirs.extend([config['openblas']['library_dir']])
       include_dirs.extend([config['openblas']['include_dir']])
-else:
-      print(f'\033[31mKeyError: cannot find the [openblas] section in site.cfg\033[0m')
+except KeyError as err:
+      print(f'\033[31mKeyError: cannot find the {err} key in the site.cfg file. See the site.cfg.example file for documentation\033[0m')
       exit(1)
 
 if sys.platform.startswith('win32'):
